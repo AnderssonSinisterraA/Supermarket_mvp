7cb5c329e04bae5ab2f4423506f3b45f677f8702
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-10-13 14:30:56 -0500

    Correciones Pay Mode

--- Presenters/Common/ModeIDataValidation.cs
+++ Presenters/Common/ModeIDataValidation.cs
@@ -5,18 +5,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Supermarket_mvp.Presenters.Common
 {
-    internal class ModeIDataValidation
+    internal class ModelDataValidation
     {
-        public void Validate(object model) 
+
+        public void Validate(object model)
         {
             string errorMessage = "";
             List<ValidationResult> validationResults = new List<ValidationResult>();
             ValidationContext validationContext = new ValidationContext(model);
             bool isValid = Validator.TryValidateObject(
                 model, validationContext, validationResults, true);
-            if (!isValid == false)
+            if (isValid == false)
             {
                 foreach (var item in validationResults)
                 {
