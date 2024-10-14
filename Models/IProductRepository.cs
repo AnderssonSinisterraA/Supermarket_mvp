ae9f1555884cbb48d4be9748aabe0cd591fabc6c
Andersson Sinisterra A <asinisterra2063@campuscecep.edu.co>
2024-10-13 22:05:35 -0500

    ProductPresenter

---
+++ Models/IProductRepository.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface IProductRepository
+    {
+        string connectionString { get; }
+
+        void Add(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO Product (Product_Name, Price, Stock, Category_Id) VALUES (@name, @price, @stock, @categoryId)";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productModel.Name;
+                command.Parameters.Add("@price", SqlDbType.Int).Value = productModel.Price;
+                command.Parameters.Add("@stock", SqlDbType.Int).Value = productModel.Stock;
+                command.Parameters.Add("@categoryId", SqlDbType.Int).Value = productModel.CategoryId;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        void Edit(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"UPDATE Product
+                                         SET Product_Name = @name,
+                                             Price = @price,
+                                             Stock = @stock,
+                                             Category_Id = @categoryId
+                                         WHERE Product_Id = @id";
+                command.Parameters.Add("@name", SqlDbType.NVarChar).Value = productModel.Name;
+                command.Parameters.Add("@price", SqlDbType.Int).Value = productModel.Price;
+                command.Parameters.Add("@stock", SqlDbType.Int).Value = productModel.Stock;
+                command.Parameters.Add("@categoryId", SqlDbType.Int).Value = productModel.CategoryId;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productModel.Id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM Product WHERE Product_Id = @id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        IEnumerable<ProductModel> GetAll()
+        {
+            var productList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("SELECT * FROM Product", connection))
+            {
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var product = new ProductModel
+                        {
+                            Id = (int)reader["Product_Id"],
+                            Name = reader["Product_Name"].ToString(),
+                            Price = (int)reader["Price"],
+                            Stock = (int)reader["Stock"],
+                            CategoryId = (int)reader["Category_Id"]
+                        };
+                        productList.Add(product);
+                    }
+                }
+            }
+            return productList;
+        }
+
+        IEnumerable<ProductModel> GetByValue(string value)
+        {
+            var productList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand("SELECT * FROM Product WHERE Product_Name LIKE @value", connection))
+            {
+                connection.Open();
+                command.Parameters.Add("@value", SqlDbType.NVarChar).Value = $"%{value}%";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var product = new ProductModel
+                        {
+                            Id = (int)reader["Product_Id"],
+                            Name = reader["Product_Name"].ToString(),
+                            Price = (int)reader["Price"],
+                            Stock = (int)reader["Stock"],
+                            CategoryId = (int)reader["Category_Id"]
+                        };
+                        productList.Add(product);
+                    }
+                }
+            }
+            return productList;
+        }
+    }
+}
--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Supermarket_mvp.Models
 {
-    internal interface ProductModel
+    internal class ProductModel
     {
+        [DisplayName("Product Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(50, MinimumLength = 3,
+            ErrorMessage = "Product name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Price")]
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 10000, ErrorMessage = "Price must be between 0.01 and 10,000")]
+        public int Price { get; set; }
+
+        [DisplayName("Stock")]
+        [Required(ErrorMessage = "Stock is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be a positive number")]
+        public int Stock { get; set; }
+
+        [DisplayName("Category Id")]
+        [Required(ErrorMessage = "Category Id is required")]
+        public int CategoryId { get; set; }
     }
 }
